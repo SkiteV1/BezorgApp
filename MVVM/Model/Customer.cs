d78eba13231353c565ba21eda5f657026c514ded
Skite <msliekerwork@outlook.com>
2025-06-27 16:39:07 +0200

    .

---
+++ MVVM/Model/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezorgApp.MVVM.Model
+{
+    public class Customer
+    {
+
+    }
+}
