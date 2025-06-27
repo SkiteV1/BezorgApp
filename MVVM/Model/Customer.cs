14186ccedf6b72391bed271a2ffd6215ea5afab8
Skite <msliekerwork@outlook.com>
2025-06-27 17:25:43 +0200

    Models added

--- MVVM/Model/Customer.cs
+++ MVVM/Model/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,19 @@ namespace BezorgApp.MVVM.Model
 {
     public class Customer
     {
-
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public int HouseNumber { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
     }
 }
---
+++ MVVM/Model/DeliveryPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezorgApp.MVVM.Model
+{
+    public class DeliveryPerson
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int WorkId { get; set; }
+    }
+}
