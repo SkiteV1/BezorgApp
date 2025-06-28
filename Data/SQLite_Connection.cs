07548cb0940c07ec5d4f8daa8c8cf1fca688fa87
Skite <msliekerwork@outlook.com>
2025-06-28 20:44:09 +0200

    Poging om LocalDb tot stand te brengen.

---
+++ Data/SQLite_Connection.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezorgApp.Data
+{
+    public class SQLite_Connection
+    {
+        public ISQLiteAsyncConnection CreateConnection()
+        {
+            return new SQLiteAsyncConnection(
+                Path.Combine(FileSystem.AppDataDirectory, "DbBezorgApp.db3"),
+                SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache  
+                );
+        }
+    }
+}
